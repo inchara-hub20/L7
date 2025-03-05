17f96566049e482a546001ad0290c89e8cd9d0b7
inchara <inchara.m@s.amity.edu>
2025-03-05 21:23:58 +0530

    ifelse statement

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+number = 10
+
+if number > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
+
+
+number = 0
+
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+
